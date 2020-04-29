efe01db164af0ddeb0ecbf5cb3ff51608d07642a
Jeroen Janssen <jeroen.janssen@gmail.com>
2020-04-29 22:38:47 +0200

    don't log in the playback callback

--- test/test_output.ts
+++ test/test_output.ts
@@ -9,7 +9,7 @@ import udp = require('dgram');
 var server = udp.createSocket('udp4');
 
 // emits when any error occurs
-server.on('error',function(error:string){
+server.on('error', function (error: string) {
   console.log('Error: ' + error);
   server.close();
 });
@@ -20,24 +20,30 @@ let contentCount = 0;
 let emptyCount = 0;
 let totalLength = 0;
 
+let logStats = false;
+
 /** this is for recording mic and playback on own output device so you can hear what you say */
 instance.SetEncodedFrameAvailableCallBack(function (b: Buffer) {
   if (b) {
-      contentCount++;
-      totalLength += b.byteLength;
+    contentCount++;
+    totalLength += b.byteLength;
 
-      instance.DecodeDataIntoPlayback(b);
+    instance.DecodeDataIntoPlayback(b);
 
+    if (logStats) {
       if (contentCount % 100 == 0) {
-          console.log(`contentCount: ${contentCount}, totalLength: ${totalLength}`);
+        console.log(`contentCount: ${contentCount}, totalLength: ${totalLength}`);
       }
+    }
   }
   else {
-      emptyCount++;
+    emptyCount++;
 
+    if (logStats) {
       if (contentCount % 100 == 0) {
-          console.log(`emptyCount: ${emptyCount}`);
+        console.log(`emptyCount: ${emptyCount}`);
       }
+    }
   }
 });
 
@@ -45,49 +51,49 @@ instance.OutputInitAndStartStream();
 instance.InputInitAndStartStream();
 
 // emits on new datagram msg
-server.on('message',function(msg: Buffer, info: udp.RemoteInfo) {
-    let clientKey = `${info.address}:${info.port}`;
-    let payload  = Rtp.payload(msg);
+server.on('message', function (msg: Buffer, info: udp.RemoteInfo) {
+  let clientKey = `${info.address}:${info.port}`;
+  let payload = Rtp.payload(msg);
 
-    //console.log(`'${clientKey}' send msg with length ${msg.length}`);
-    if (!rtpClients.has(clientKey)) {
-      console.log(`NEW client detected at: ${clientKey}`);
-      let rtpClient = new Rehearse20(clientKey);
-      let x = rtpClient.OutputInitAndStartStream();
-      console.log(`OutputInitAndStartStream: ${x}`);
-      rtpClients.set(clientKey, rtpClient);
-    }
-    let currentClient = rtpClients.get(clientKey);
-    if (currentClient) {
-      let status = currentClient.DecodeDataIntoPlayback(payload);
-    }
+  //console.log(`'${clientKey}' send msg with length ${msg.length}`);
+  if (!rtpClients.has(clientKey)) {
+    console.log(`NEW client detected at: ${clientKey}`);
+    let rtpClient = new Rehearse20(clientKey);
+    let x = rtpClient.OutputInitAndStartStream();
+    console.log(`OutputInitAndStartStream: ${x}`);
+    rtpClients.set(clientKey, rtpClient);
+  }
+  let currentClient = rtpClients.get(clientKey);
+  if (currentClient) {
+    let status = currentClient.DecodeDataIntoPlayback(payload);
+  }
 
-    /*
-    console.log(`version        : ${Rtp.version(msg)}`);
-    console.log(`extension      : ${Rtp.extension(msg)}`);
-    console.log(`payloadType    : ${Rtp.payloadType(msg)}`);
-    console.log(`sequenceNumber : ${Rtp.sequenceNumber(msg)}`);
-    console.log(`timestamp      : ${Rtp.timestamp(msg)}`);
-    console.log(`sSrc           : ${Rtp.sSrc(msg)}`);
-    console.log(`payload.length : ${payload.byteLength}`);
-    */
-    //console.log(`status: ${status}`);
+  /*
+  console.log(`version        : ${Rtp.version(msg)}`);
+  console.log(`extension      : ${Rtp.extension(msg)}`);
+  console.log(`payloadType    : ${Rtp.payloadType(msg)}`);
+  console.log(`sequenceNumber : ${Rtp.sequenceNumber(msg)}`);
+  console.log(`timestamp      : ${Rtp.timestamp(msg)}`);
+  console.log(`sSrc           : ${Rtp.sSrc(msg)}`);
+  console.log(`payload.length : ${payload.byteLength}`);
+  */
+  //console.log(`status: ${status}`);
 });
 
 //emits when socket is ready and listening for datagram msgs
-server.on('listening',function(){
-    var address = server.address();
-    var port = address.port;
-    var family = address.family;
-    var ipaddr = address.address;
-    console.log('Server is listening at port' + port);
-    console.log('Server ip :' + ipaddr);
-    console.log('Server is IP4/IP6 : ' + family);
-  });
-  
-  //emits after the socket is closed using socket.close();
-  server.on('close',function(){
-    console.log('Socket is closed !');
-  });
-  
-  server.bind(2222);
+server.on('listening', function () {
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close', function () {
+  console.log('Socket is closed !');
+});
+
+server.bind(2222);
